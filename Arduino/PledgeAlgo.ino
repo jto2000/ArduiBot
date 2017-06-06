41d3c96615898d7d18c173affad88c20dcebbcd1
jto2000 <j_to2000@hotmail.com>
2017-06-06 16:22:14 +0200

    Upload sketches for Bot

---
+++ Arduino/PledgeAlgo.ino
@@ -0,0 +1,78 @@
+int DetectRoom()
+{
+  int status = 0; // Status of detection
+  float cumulatedAngle = 0.0; // Angle cumulated for pledge Algo :
+  boolean direction = false; //(TRUE = right / false = Left)
+
+  // 0 Init : Go forward
+  // 1 When wall detected, go to calculated  distance
+  // 2 Rotate and save angle
+  // 3 Follow wall
+  // if wall detected front (both US)
+  // Rotate desired side (go to 2)
+  // if cumulatedAngle == 0 => Go front
+    
+    }
+
+int FollowWall(boolean WallAtRight, float distSetPoint)
+{
+  // Compare la distance au mur sélectionné (gauche ou droit) au SetPoint, et donne un nouveau cap a tenir en fonction.
+  // Dans cette version, pas de surveillance de la distance opposée
+  int headingStep = 5; // Step grossier en degré
+  int newHeading;
+  // L'algo doit conserver sa distance par rapport au mur qu'il longe :
+  // définir la distance de suivi et le côté !
+  // WallAtRight = true if following wall on the right side, false else => (-1)*0 = 1 dir trigo !
+  
+  // Obtenir distance adj et opposée
+    float distWall, distDummy;
+
+  
+  /*if (WallAtRight)
+  {
+    float distWall = MesureUS(1);
+    float distDummy = MesureUS(2);
+  }
+  else
+  {
+    float distWall = MesureUS(2);
+    float distDummy = MesureUS(1);
+  }*/
+  distWall = WallAtRight ? MesureUS(1) : MesureUS(2);
+  //distDummy = WallAtRight ? MesureUS(2) : MesureUS(1);
+  
+  // Alerte sur distDummy ?
+
+  
+  // 2. Si mesure > cote : tourner à d
+  // si mesure < cote tourner à g
+
+  // Si (distance - setpoint) < setpoint / 2 => On considère OK pour éviter itérations
+  if (abs(distWall - distSetPoint) <= (distSetPoint / 2))
+  {
+    return 0;
+  }
+  
+  // newHeading = (-1^direction)*(-1^supdistance)*headingStep
+  if (distWall > distSetPoint)
+  {
+    //mur loin, on s'en rapproche
+    newHeading = (-1<<WallAtRight)*headingStep;
+  }
+  else
+  {
+    //mur proche, on s'en éloigne
+    newHeading = - (-1<<WallAtRight)*headingStep;
+    // void Drive(int cap, int speed)
+  }
+  return newHeading;
+
+  
+  // V1 en mode on/off +10° ?
+  // V2 en PID ?
+}
+
+
+// ajout variables globales :
+// Vitesse, heading, heading_désiré, vitesse désirée ?
+// Distance gauche et distance droite ?
---
+++ Arduino/motorDrive.ino
@@ -0,0 +1,202 @@
+void Drive(int cap, int speed)
+{
+  // Choisit la direction à donner et la vitesse :
+  // Direction en degrés
+  // Vitesse en %
+  // Si droite
+  // Si gauche
+  // Si abs() > 90
+  // on arrête une roue
+  
+  
+  // @ Ajouter notion point fixe, cherche direction
+  
+  int LeftSpeed, RightSpeed;
+  float Repart;
+  
+  if (speed == 0)
+  {
+     motor(1, RELEASE, 0);
+     motor(2, RELEASE, 0);
+  }
+  else
+  {
+    // Calcul de la vitesse des moteurs :
+    // Vitesse = Vref * direction !
+    // Approx : 
+    // Si cap = 0 (tout droit) :
+    // deux moteurs à 100% de speed
+    // Si cap = 90 (à droite)
+    // Moteur droit à 0; Mgauche à 100% de speed
+    //capRad = abs(cap)*pi()/2;
+    Repart = cos(abs(cap)*pi/2);
+    if (cap > 0)
+    {
+      LeftSpeed = (255*speed/100)*Repart;
+      RightSpeed = (255*speed/100)*(1-Repart);
+    }
+    else
+    {
+      LeftSpeed = (255*speed/100)*(1-Repart);
+      RightSpeed = (255*speed/100)*Repart;
+    }
+    // Si switch % négatif, s'arrêter d'abord
+    motor(1, FORWARD, LeftSpeed); 
+    motor(2, FORWARD, RightSpeed); 
+    // Possibilité de réécrire sous fonction en int plutôt qu'en direction ?
+  } 
+}
+
+
+//=== Fonction motor
+// Choisir le moteur (1-4), la commande et la vitesse (0-255).
+// Les commandes sont : FORWARD, BACKWARD, BRAKE, RELEASE.
+void motor(int nMotor, int command, int speed)
+{
+  int motorA, motorB;
+
+  if (nMotor >= 1 && nMotor <= 4)
+  {  
+    switch (nMotor)
+    {
+    case 1:
+      motorA   = MOTOR1_A;
+      motorB   = MOTOR1_B;
+      break;
+    case 2:
+      motorA   = MOTOR2_A;
+      motorB   = MOTOR2_B;
+      break;
+    case 3:
+      motorA   = MOTOR3_A;
+      motorB   = MOTOR3_B;
+      break;
+    case 4:
+      motorA   = MOTOR4_A;
+      motorB   = MOTOR4_B;
+      break;
+    default:
+      break;
+    }
+
+    switch (command)
+    {
+    case FORWARD:   //Tourner en avant
+      motor_output (motorA, HIGH, speed);
+      motor_output (motorB, LOW, -1);     // -1: no PWM set
+      break;
+    case BACKWARD:  //Tourner en arrière
+      motor_output (motorA, LOW, speed);
+      motor_output (motorB, HIGH, -1);    // -1: no PWM set
+      break;
+    case BRAKE:   //Freiner
+      motor_output (motorA, LOW, 255); // 255: fully on.
+      motor_output (motorB, LOW, -1);  // -1: no PWM set
+      break;
+    case RELEASE:   //Stop
+      motor_output (motorA, LOW, 0);  // 0: output floating.
+      motor_output (motorB, LOW, -1); // -1: no PWM set
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+
+//=== Fonction motor_output
+// Utilise le driver pour piloter des sorties
+// Mettre la  variable high_low sur HIGH / LOW pour des lampes
+// On une speed = 0
+// speed varie de 0-255 pour les 2 pins (0 = arrêt, 255 = maxi)
+// à mettre sur -1 pour ne pas régler de PWM du tout
+void motor_output (int output, int high_low, int speed)
+{
+  int motorPWM;
+
+  switch (output)
+  {
+  case MOTOR1_A:
+  case MOTOR1_B:
+    motorPWM = MOTOR1_PWM;
+    break;
+  case MOTOR2_A:
+  case MOTOR2_B:
+    motorPWM = MOTOR2_PWM;
+    break;
+  case MOTOR3_A:
+  /*case MOTOR3_B:
+    motorPWM = MOTOR3_PWM;
+    break;
+  case MOTOR4_A:
+  case MOTOR4_B:
+    motorPWM = MOTOR4_PWM;
+    break;*/
+  default:
+    // Utilise speed comme flag d'erreur, -3333 = invalid output.
+    speed = -3333;
+    break;
+  }
+
+  if (speed != -3333)   //La valeur speed est valide
+  {
+    // Set the direction with the shift register 
+    // on the MotorShield, even if the speed = -1.
+    // In that case the direction will be set, but not the PWM.
+    shiftWrite(output, high_low);
+
+    // Ajuster le PWM seulemernt s il est valide
+    if (speed >= 0 && speed <= 255)    
+    {
+      analogWrite(motorPWM, speed);
+    }
+  }
+}
+
+
+// Fonction shiftWrite
+// The parameters are just like digitalWrite().
+// The output is the pin 0...7 (the pin behind the shift register).
+// The second parameter is HIGH or LOW.
+void shiftWrite(int output, int high_low)
+{
+  static int latch_copy;
+  static int shift_register_initialized = false;
+
+  // Do the initialization on the fly, 
+  // at the first time it is used.
+  if (!shift_register_initialized)
+  {
+    // Set pins for shift register to output
+    pinMode(MOTORLATCH, OUTPUT);
+    pinMode(MOTORENABLE, OUTPUT);
+    pinMode(MOTORDATA, OUTPUT);
+    pinMode(MOTORCLK, OUTPUT);
+
+    // Set pins for shift register to default value (low);
+    digitalWrite(MOTORDATA, LOW);
+    digitalWrite(MOTORLATCH, LOW);
+    digitalWrite(MOTORCLK, LOW);
+    // Enable the shift register, set Enable pin Low.
+    digitalWrite(MOTORENABLE, LOW);
+
+    // start with all outputs (of the shift register) low
+    latch_copy = 0;
+
+    shift_register_initialized = true;
+  }
+
+  // The defines HIGH and LOW are 1 and 0.
+  // So this is valid.
+  bitWrite(latch_copy, output, high_low);
+
+  // Use the default Arduino 'shiftOut()' function to
+  // shift the bits with the MOTORCLK as clock pulse.
+  // The 74HC595 shiftregister wants the MSB first.
+  // After that, generate a latch pulse with MOTORLATCH.
+  shiftOut(MOTORDATA, MOTORCLK, MSBFIRST, latch_copy);
+  delayMicroseconds(5);    // For safety, not really needed.
+  digitalWrite(MOTORLATCH, HIGH);
+  delayMicroseconds(5);    // For safety, not really needed.
+  digitalWrite(MOTORLATCH, LOW);
+}
