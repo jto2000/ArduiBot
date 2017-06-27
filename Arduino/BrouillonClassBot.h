d884fa8f3ea28389ec6767027b1daa12fe602a54
jto2000 <j_to2000@hotmail.com>
2017-06-27 11:30:43 +0200

    Create BrouillonClassBot.h

--- Arduino/BrouillonClassBot.h
+++ Arduino/BrouillonClassBot.h
@@ -1,1 +1,36 @@
 
+#include "USlib.h"
+
+class Robot
+{
+	public;
+	
+	Robot(); // Constructeur
+	void DetectRoom();
+	void returnHome();
+	void discoverSurround(); // Discover direct surrounding by rotating n-times 360°
+	void discoverRoom(); // Discover room
+	
+	
+	private;
+	
+	// Variables :
+	int wheelRadius;
+	int botWidth;
+	int minMotorSpeed;
+	int xPos;
+	int yPos;
+	
+	//Autres objets :
+	// Sensors
+	// sensorUS sensorLeft;
+	// sensorUS sensorRight;
+	// motors
+	
+	// Méthodes privees :
+	int gotoWall(); // Output distance from wall
+	void minSpeedDetermination(); // v mini 
+	void faceWall();
+	int gotoxy(int xtarge, int ytarget);
+	
+}
