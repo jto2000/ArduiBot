5a405613f523783fa0d25c31effa9e74139ed1ef
jto2000 <j_to2000@hotmail.com>
2017-06-27 15:04:26 +0200

    Create botDrive.h

---
+++ Arduino/botDrive.h
@@ -0,0 +1,15 @@
+class botDrive
+{
+  public;
+  
+  botDrive();
+  void move(int Vud, int theta, int dist);
+  // with tangent speed, direction and distance to stop
+  
+  private;
+    d_TimeToStop;
+    void shiftWrite(int output, int high_low);
+    void motor_output (int output, int high_low, int speed);
+  
+  
+}
